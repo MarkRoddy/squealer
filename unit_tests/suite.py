b4c2ba0bcf1fc1b7b8364ac9542500cb1c0494e9
Mark Roddy <markroddy@gmail.com>
2011-05-21 02:43:57 -0400

    Adding a suite script to make it easier to run all the tests at once.

---
+++ unit_tests/suite.py
@@ -0,0 +1,10 @@
+
+from squealer import main
+
+from unit_tests.test_examples import *
+from unit_tests.test_pigproxy import *
+from unit_tests.test_gruntparser import *
+from unit_tests.test_pigtest import *
+
+if __name__ == '__main__':
+    main()
