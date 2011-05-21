27e396f6a8f6dee04f1a604cd02a6748d8e51bb8
Mark Roddy <markroddy@gmail.com>
2011-05-21 00:02:45 -0400

    Completed implementation of remaining methods on the test case class.

--- squealer/pigtest.py
+++ squealer/pigtest.py
@@ -25,7 +25,7 @@ class PigTest(unittest.TestCase):
         arglist = ["%s=%s"%(k,v) for (k,v) in self.Args.iteritems()]
         self._proxy = PigProxy.fromFile(self.PigScript, arglist)
 
-    def get_relation(self, alias):
+    def relation(self, alias):
         """
         Returns the data in the specified relation, converted to
         python types
@@ -35,25 +35,22 @@ class PigTest(unittest.TestCase):
             output.append(self._to_python(t))
         return output
 
+    def last_stored(self):
+        """
+        Returns the name of the relation that was last stored
+        in the pig script
+        """
+        self._proxy.register_script()
+        return self._proxy.alias_overrides["LAST_STORE_ALIAS"]
+
     def _to_python(self, value):
         """Converts a pig/java data type to its python equivilant"""
         if isinstance(value, PigTuple):
             return tuple([self._to_python(v) for v in value.getAll()])
         return value
-        
-    def assertRelationEquals(self, alias, expected):
-        """Assert that the specified alias has the expected set of records"""
-        actual = self.get_relation(alias)
-        self.assertEqual(expected, actual)
-
-    def assertLastStoreEquals(self, expected):
-        """Assert that the alias in the last STORE operation of the script had the expected records"""
-        self._proxy.register_script()
-        alias = self._proxy.alias_overrides["LAST_STORE_ALIAS"]
-        self.assertRelationEquals(alias, expected)
 
     def override_data(self, alias, data):
-        """Override the data in a relation with that specified"""
+        """Override the data in a relation with the records specified"""
         self._proxy.overrideToData(alias, data)
 
     def override_query(self, alias, query):
@@ -71,3 +68,34 @@ class PigTest(unittest.TestCase):
         query: The new value of the alias.
         """
         self._proxy.override(alias, query)
+        
+    def assertRelationEquals(self, alias, expected):
+        """Assert that the specified alias has the expected set of records"""
+        actual = self.relation(alias)
+        self.assertEqual(expected, actual)
+
+    def assertLastStoreEquals(self, expected):
+        """Assert that the alias in the last STORE operation of the script had the expected records"""
+        alias = self.getLastStoredRelationName()
+        self.assertRelationEquals(alias, expected)
+
+    def assertLastStoreEqualsFile(self, file_path, sep = ','):
+        """Assert that the alias in the last STORE operation of the script is equal to data in file"""
+        relation_name = self.getLastStoredRelationName()
+        actual_records = self.relation(relation_name)
+        first_actual_record = actual_records[0]
+        type_list = map(lambda x: x.__class__, first_actual_record)
+        fobj = open(file_path)
+        expected_records = []
+        try:
+            for r in fobj:
+                str_vals = r.split(sep)
+                casted_vals = []
+                for i,v in enumerate(str_vals):
+                    t = type_list[i]
+                    casted_vals.append(t(v))
+                expected_records.append( tuple(casted_vals) )
+        finally:
+            fobj.close()
+        self.assertEqual(expected_records, actual_records)
+
--- unit_tests/test_pigtest.py
+++ unit_tests/test_pigtest.py
@@ -2,7 +2,6 @@
 import unittest
 from squealer.pigtest import PigTest
 
-
 class TestPigTest(unittest.TestCase):
 
     PIG_SCRIPT = "pig-scripts/top_queries.pig"
@@ -26,8 +25,6 @@ class TestPigTest(unittest.TestCase):
                 self.assertRelationEquals("queries_limit", output)
         test = some_test('testNoToN')
         test.testNoToN()
-
-
 
     def testImplicitNtoN(self):
         class some_test(PigTest):
@@ -131,84 +128,24 @@ class TestPigTest(unittest.TestCase):
         test = some_test('testOverride')
         test.testOverride()
             
+    def testFileOutput(self):
+        class some_test(PigTest):
+            Args = {
+                "n" : 3,
+                "reducers" : 1,
+                "input" : self.INPUT_FILE,
+                "output" : "top_3_queries",
+                }
+            PigScript = self.PIG_SCRIPT
+            def testFileOutput(self):
+                fobj = open("test-data/top_queries_expected_top_3.txt")
+                try:
+                    self.assertLastStoreEqualsFile(fobj)
+                finally:
+                    fobj.close()
+        test = some_test('testFileOutput')
+        test.testFileOutput()
 
-
-#     def testFileOutput(self):
-#         args = [
-#             "n=3",
-#             "reducers=1",
-#             "input=" + self.INPUT_FILE,
-#             "output=top_3_queries",
-#             ]
-#         test = PigProxy.fromFile(self.PIG_SCRIPT, args)
-#         fobj = open("test-data/top_queries_expected_top_3.txt")
-#         try:
-#             test.assertOutputEqualsFile(fobj)
-#         finally:
-#             fobj.close()
-
-#     def testGetLastAlias(self):
-#         script = '\n'.join([
-#             "data = LOAD '%s' AS (query:CHARARRAY, count:INT);" % self.INPUT_FILE,
-#             "queries_group = GROUP data BY query PARALLEL 1;",
-#             "queries_sum = FOREACH queries_group GENERATE group AS query, SUM(data.count) AS count;",
-#             "queries_ordered = ORDER queries_sum BY count DESC PARALLEL 1;",
-#             "queries_limit = LIMIT queries_ordered 3;",
-#             "STORE queries_limit INTO 'top_3_queries';",
-#             ])
-#         test = PigProxy(script)
-#         expected = \
-#         "(yahoo,25L)\n" + \
-#         "(facebook,15L)\n" + \
-#         "(twitter,7L)"
-#         self.assertEquals(expected, '\n'.join([str(i) for i in test.get_alias("queries_limit")]))
-
-#     def testWithUdf(self):
-#         script = '\n'.join([
-#             # "REGISTER myIfNeeded.jar;",
-#             "DEFINE TOKENIZE TOKENIZE();",
-#             "data = LOAD '%s' AS (query:CHARARRAY, count:INT);" % self.INPUT_FILE,
-#             "queries = FOREACH data GENERATE query, TOKENIZE(query) AS query_tokens;",
-#             "queries_ordered = ORDER queries BY query DESC PARALLEL 1;",
-#             "queries_limit = LIMIT queries_ordered 3;",
-#             "STORE queries_limit INTO 'top_3_queries';",
-#             ])
-#         test = PigProxy(script);
-#         output = [
-#             "(yahoo,{(yahoo)})",
-#             "(yahoo,{(yahoo)})",
-#             "(twitter,{(twitter)})",
-#             ]
-#         test.assertLastOutput(output);
-
-
-
-#     def testArgFiles(self):
-#         argsFile = [
-#             "test-data/top_queries_params.txt"
-#             ]
-#         test = PigProxy.fromFile(self.PIG_SCRIPT, arg_files = argsFile)
-#         fobj = open("test-data/top_queries_expected_top_3.txt")
-#         try:
-#             test.assertOutputEqualsFile(fobj)
-#         finally:
-#             fobj.close()
-
-
-
-#     def testStore(self):
-#         args = [
-#             "n=3",
-#             "reducers=1",
-#             "input=" + self.INPUT_FILE,
-#             "output=top_3_queries",
-#             ]
-#         test = PigProxy.fromFile(self.PIG_SCRIPT, args)
-
-#         # By default all STORE and DUMP commands are removed
-#         test.unoverride("STORE")
-#         test.run_script()
-#         self.assertTrue(test.cluster.delete(Path("top_3_queries")))
 
 if __name__ == '__main__':
     unittest.main()
